7e9a9bc3d104280d61ae243fab00269acb0c1fe8
Tathya Dixit <dixit.tathya2003@gmail.com>
2026-02-22 16:02:25 +0530

    fix : minor fixes

--- expense/models.py
+++ expense/models.py
@@ -9,7 +9,7 @@ class Type(models.TextChoices):
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     type_choice = models.CharField(max_length=2, choices=Type.choices, default=Type.EXPENSE)
 
     is_predefined = models.BooleanField(default=False)
@@ -28,15 +28,15 @@ class Transaction(models.Model):
     
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     type_choice = models.CharField(max_length=2, choices=Type.choices, default=Type.EXPENSE)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     payment_method = models.CharField(max_length=3, choices=PaymentMethod.choices)
     category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True, related_name='transactions')
-    transaction_date = models.DateTimeField(auto_now_add=True)
+    transaction_date = models.DateTimeField()
     
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
     created_at = models.DateTimeField(auto_now_add=True)
     is_deleted = models.BooleanField(default=False)
-    deleted_at = models.DateTimeField()
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
 
 class Budget(models.Model):
