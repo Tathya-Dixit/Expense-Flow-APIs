3f9473066718f56638bf3a6532c95902a63355b2
Tathya Dixit <dixit.tathya2003@gmail.com>
2026-03-02 14:28:08 +0530

    fix : minor fixes

--- expense_flow/settings.py
+++ expense_flow/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
